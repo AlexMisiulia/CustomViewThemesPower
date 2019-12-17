d9b9ab322f6eb299fc8e0213776cd5518934bce9
Alexey Misiulia <alexeymisiulia@gmail.com>
2019-12-17 22:47:10 +0300

    complete sample

---
+++ app/src/main/java/com/sharyfire/widgetthemespower/TextTextView.kt
@@ -0,0 +1,30 @@
+package com.sharyfire.widgetthemespower
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.widget_text_text_view.view.*
+
+class TextTextView: LinearLayout {
+
+    constructor(context: Context?) : super(context) {
+        init()
+    }
+
+    constructor(context: Context?, attrs: AttributeSet?) : super(context, attrs) {
+        init()
+    }
+
+    private fun init() {
+        LayoutInflater.from(context).inflate(R.layout.widget_text_text_view, this)
+    }
+
+    fun setText1(text: String) {
+        text1TextView.text = text
+    }
+
+    fun setText2(text: String) {
+        text2TextView.text = text
+    }
+}
